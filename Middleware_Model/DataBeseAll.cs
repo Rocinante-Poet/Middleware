8ad4cb0a78a2762feb702d956ee9a85a04f1fb60
WindCloud <YangkunJuly@outlook.com>
2019-11-04 08:45:52 +0800

    添加项目文件。

---
+++ Middleware_Model/DataBeseAll.cs
@@ -0,0 +1,63 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Middleware_Model
+{
+    public class DataBeseAll
+    {
+    }
+
+    [Table("logstr")]
+    public class Log4Info
+    {
+        [Key]
+        [Column("logId")]
+        public int Id { get; set; }
+
+        public string logLevel { get; set; }
+        public string logMessage { get; set; }
+    }
+
+    public class Operatinginfo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int UserID { get; set; }
+        public string Operating { get; set; }
+        public string Date { get; set; }
+        public string Details { get; set; }
+    }
+
+    public class Powerdetails
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public int PowerGroupID { get; set; }
+        public string Function { get; set; }
+    }
+
+    public class Powerinfo
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public string Power { get; set; }
+        public string PowerName { get; set; }
+        public string PowerDetails { get; set; }
+    }
+
+    public class Userinfo
+    {
+        [Key]
+        public int UserID { get; set; }
+
+        public string Name { get; set; }
+        public string Remark { get; set; }
+        public int Power_ID { get; set; }
+        public string UserState { get; set; }
+    }
+}
---
+++ Middleware_Tool/Log4net.cs
@@ -0,0 +1,62 @@
+﻿using log4net;
+using log4net.Config;
+using log4net.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Middleware_Tool
+{
+    public class Log4net
+    {
+        private static Dictionary<string, log4net.ILog> m_lstLog = new Dictionary<string, log4net.ILog>();
+
+        static Log4net()
+        {
+            string strLog4NetConfigFile = AppDomain.CurrentDomain.BaseDirectory + "log4net.config";
+            ILoggerRepository repository = LogManager.CreateRepository("NETCorelog4net");
+            XmlConfigurator.Configure(repository, new System.IO.FileInfo(strLog4NetConfigFile));
+            m_lstLog["info_log"] = LogManager.GetLogger(repository.Name, "info_log");
+            m_lstLog["error_log"] = LogManager.GetLogger(repository.Name, "error_log");
+            m_lstLog["mysql_log"] = LogManager.GetLogger(repository.Name, "mysql_log");
+        }
+
+        /// <summary>
+        /// 功能描述:写入常规日志
+        /// </summary>
+        /// <param name="strInfoLog">strInfoLog</param>
+        public static void WriteInfo(string strInfoLog)
+        {
+            if (m_lstLog["info_log"].IsInfoEnabled)
+            {
+                m_lstLog["info_log"].Info(strInfoLog);
+            }
+        }
+
+        /// <summary>
+        /// 功能描述:写入错误日志
+        /// </summary>
+        /// <param name="strErrLog">strErrLog</param>
+        /// <param name="ex">ex</param>
+        public static void WriteError(string strErrLog, Exception ex = null)
+        {
+            if (m_lstLog["error_log"].IsErrorEnabled)
+            {
+                m_lstLog["error_log"].Error(strErrLog, ex);
+            }
+        }
+
+        /// <summary>
+        /// 功能描述:写入MySql日志
+        /// </summary>
+        /// <param name="">strInfoLog</param>
+        public static void WriteMysql(string level, string Message)
+        {
+            if (m_lstLog["mysql_log"].IsErrorEnabled)
+            {
+                GlobalContext.Properties["loglevel"] = $"{level}";
+                m_lstLog["mysql_log"].Info($"{Message}");
+            }
+        }
+    }
+}
